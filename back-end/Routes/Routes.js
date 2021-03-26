65699ff9906a0e474c739c55154f82b385160818
bakary <bakarykone729@gmail.com>
2021-03-26 15:36:41 +0100

    prisma commit

---
+++ back-end/Routes/Routes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express.Router();
+
+
+
+api.get('/', async (req, res) => {
+    try {
+  
+      const prisma = new PrismaClient()
+      const users = await prisma.user.findMany({
+        select: {
+          firstname: true,
+          lastname: true,
+          email: true,
+          createdAt: true,
+          updatedAt: true
+        }
+      })
+  
+      res.json({ data: { users } })
+    } catch (err) {
+      res.status(BAD_REQUEST.status).json({ error: err.message })
+    }
+  })
+  
+  api.put('/:id', async (req, res) => {
+    try {
+      const id = parseInt(req.params.id, 10)
+  
+      const prisma = new PrismaClient()
+      const user = await prisma.user.findFirst({
+        where: {
+          id
+        }
+      })
+  
+      if (!user) {
+        return res.status(BAD_REQUEST.status).json({ error: `User ${id} doesn't exist` })
+      }
+  
+      const acceptedFields = ['firstname', 'lastname', 'gender']
+      let data = {}
+      for (const key of acceptedFields) {
+        if (req.body[key]) {
+          data[key] = req.body[key]
+        }
+      }
+  
+      const updatedUser = await prisma.user.update({
+        where: {
+          id
+        },
+        data
+      })
+  
+      res.json({ data: { user: updatedUser } })
+    } catch (err) {
+      res.status(BAD_REQUEST.status).json({ error: err.message })
+    }
+  })
+  
+  api.delete('/:id', (req, res) => {
+  })
+  
+  export default api
---
+++ back-end/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.listen(5500, () => {
+    console.log('Server is listening on https://localhost:5500')
+}) 
